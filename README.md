9523e8970b6f4bd247dda82a12a14a1dd60a8b6f
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2026-01-21 12:06:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# components
+
+HTML components for the go web framework tinywasm
---
+++ components.go
@@ -0,0 +1,7 @@
+package components
+
+type Components struct {}
+
+func New() *Components {
+    return &Components{}
+}
